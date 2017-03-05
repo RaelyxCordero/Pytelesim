d62121bcefba70c2f68a241ea811d4f30531898c
Daniel Klie <danielnswz@gmail.com>
2017-03-05 01:33:44 -0400

    plotting bug fixes.. still working on it

--- modulacion/static/components/js/custom.js
+++ modulacion/static/components/js/custom.js
@@ -1,8 +1,21 @@
-function draw(fun) {
+var width = 800;
+var height = 400;
+function computeYScale (width, height, xScale) {
+  var xDiff = xScale[1] - xScale[0]
+  var yDiff = height * xDiff / width
+  return [-yDiff / 2, yDiff / 2]
+}
+function drawModuladora(fun, T, A) {
     var eq = $('#eq-' + fun).val();
     console.log('Drawing '+fun+': '+eq);
     try {
-        var func = functionPlot({
+        var moduladora = functionPlot({
+            width: width,
+            height: height,
+            xDomain: [-3*T, 3*T],
+            yDomain: [-A,A],
+            xLabel: 'Frecuencia(Hz)',
+            yLabel: 'Amplitud(V)',
             target: '#plot-' + fun,
             grid: true,
             data: [{
@@ -11,7 +24,59 @@ function draw(fun) {
                 graphType: 'polyline'
             }]
         });
-        func.programmaticZoom([0, 1e-1], [-10, 10]); //Para definir el zoom de la grafica
+        console.log('intancia moduladora '+ moduladora.id);
+        //moduladora.programmaticZoom([0, 2*T], [-A, A]); //Para definir el zoom de la grafica
+    } catch (err) {
+        console.log(err);
+        alert(err);
+    }
+}
+function drawPortadora(fun, T, A) {
+    var eq = $('#eq-' + fun).val();
+    console.log('Drawing '+fun+': '+eq);
+    try {
+        var portadora = functionPlot({
+            width: width,
+            height: height,
+            xDomain: [-3*T, 3*T],
+            yDomain: [-A,A],
+            xLabel: 'Frecuencia(Hz)',
+            yLabel: 'Amplitud(V)',
+            target: '#plot-' + fun,
+            grid: true,
+            data: [{
+                fn: $('#eq-' + fun).val(),
+                sampler: 'builtIn', // this will make function-plot use the evaluator of math.js
+                graphType: 'polyline'
+            }]
+        });
+        console.log('intancia portadora '+ portadora.id);
+        //portadora.programmaticZoom([0, 2*T], [-A, A]); //Para definir el zoom de la grafica
+    } catch (err) {
+        console.log(err);
+        alert(err);
+    }
+}
+function drawModulada(fun, T, A) {
+    eq = $('#eq-' + fun).val();
+    console.log('Drawing '+fun+': '+eq);
+    try {
+        var modulada = functionPlot({
+            width: width,
+            height: height,
+            xDomain: [-3*T, 3*T],
+            yDomain: [-4,4],
+            xLabel: 'Frecuencia(Hz)',
+            yLabel: 'Amplitud(V)',
+            target: '#plot-' + fun,
+            grid: true,
+            data: [{
+                fn: $('#eq-modulada').val(),
+                sampler: 'builtIn', // this will make function-plot use the evaluator of math.js
+                graphType: 'polyline'
+            }]
+        });
+        //modulada.programmaticZoom([0, 2*T], [-A, A]); //Para definir el zoom de la grafica
     } catch (err) {
         console.log(err);
         alert(err);
@@ -46,7 +111,7 @@ function modulate() {
         success: function(data) {
             $('#eq-moduladora').val(data.moduladora);
             $('#eq-portadora').val(data.portadora);
-            $('#eq-modulada').val(data.modulada);
+            //$('#eq-modulada').val(data.modulada);
             $('#vc_modulada').val(vc);
             $('#vct_modulada').val(vct);
             $('#fc_modulada').val(fc);
@@ -54,9 +119,9 @@ function modulate() {
             $('#vmt_modulada').val(vmt);
             $('#fm_modulada').val(fm);
             console.log(data);
-            draw('moduladora');
-            draw('portadora');
-            draw('modulada');
+            //drawModuladora('moduladora', 1/fm, vm);
+            //drawPortadora('portadora', 1/fc, vc);
+            drawModulada('modulada', 1/fc+1/fm, vc);
         }
     });
 }
